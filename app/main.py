21ae97090f574605a683dcc3e336a3547eb21f53
Davi Andrade <davi.andrade@55pbx.com.br>
2023-11-10 15:56:16 -0300

    fix(color): modificado a cor do texto de saudação para o azul

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ import tkinter as tk
 
 window = tk.Tk()
 
-greeting = tk.Label(text="Hello, Tkinter")
+greeting = tk.Label(
+        text="Hello, Tkinter",
+        foreground='blue',
+        background='white'
+        )
 greeting.pack()
 
 window.mainloop()
