1934400684e758bb82c256f49e13d8b4cba586d8
Davi Andrade <davi.andrade@55pbx.com.br>
2023-11-10 17:18:19 -0300

    new(novidade): criado o botão com a informação do duende verde

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from selenium.webdriver import Chrome
+from time import sleep
 
 window = tk.Tk()
 
@@ -8,6 +9,16 @@ greeting = tk.Label(
         foreground='blue',
         background='white'
         )
+
+def openTabGoogle():
+    service = Chrome()
+    tab = service.get("https://pt.wikipedia.org/wiki/Duende_Verde")
+    sleep(10)
+    tab.quit()
+    
+button = tk.Button(window,text="Spider man 3", command=openTabGoogle )
+button.place(x=50, y=50)
+
 greeting.pack()
 
 window.mainloop()
